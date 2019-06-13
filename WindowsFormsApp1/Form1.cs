a5ede6e2204a74e9c1ad34d6409b05525aeedc4a
Jacob Tinning <jacobtinning@mmc.school.nz>
2019-06-14 10:35:09 +1200

    testing the commit

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -12,9 +12,35 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        Graphics g;
+     
+        List<Grid_Items> floor = new List<Grid_Items>();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //get the graphics used to paint on the panel control
+            g = e.Graphics;
+            //call the Planet class's DrawPlanet method to draw the image planet1 
+            for (int i = 0; i < 7; i++)
+            {
+                for (int l = 0; l < 7; i++)
+                {
+                    floor[i] = new Grid_Items();
+                }
+
+            }
+
+
         }
     }
 }
---
+++ WindowsFormsApp1/Grid-Items.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace WindowsFormsApp1
+{
+    class Grid_Items
+    {
+        private int x, y, width, height;
+        private Image Floor_Image;
+        private Rectangle FloorRec;
+
+        public Grid_Items()
+            {
+            x = 0;
+            y = 0;
+            height = 50;
+            width = 50;
+            Floor_Image = Properties.Resources.Base_Grid_Item;
+            FloorRec = new Rectangle(x, y, width, height);
+
+        }
+        public void DrawMap(Graphics g)
+        {
+            g.DrawImage(Floor_Image, FloorRec);
+        }
+
+    }
+}
